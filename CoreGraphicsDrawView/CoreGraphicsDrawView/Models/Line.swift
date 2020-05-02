7c2add6147e2482ca295021c2817ee6e8ea869b4
harut <harut.yeremyan1994@gmail.com>
2020-05-02 11:26:12 +0400

    CoreGraphicsDrawView

---
+++ CoreGraphicsDrawView/CoreGraphicsDrawView/Models/Line.swift
@@ -0,0 +1,16 @@
+//
+//  Line.swift
+//  CoreGraphicsDrawView
+//
+//  Created by Harut on 5/2/20.
+//  Copyright © 2020 Harut Yeremyan. All rights reserved.
+//
+
+import UIKit
+
+struct Line {
+    
+    let srtokeWidth: Float
+    let color: UIColor
+    var point: [CGPoint ]
+}
